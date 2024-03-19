25467eaf7203492a8c222f7b0abbfee9f879562d
Rodrigo Faria <jrfaria@ua.pt>
2024-03-19 15:42:39 +0000

    updates to the properly written code

--- labs/lab2/main/source.c
+++ labs/lab2/main/source.c
@@ -17,6 +17,25 @@
 #define DISPLAY0 0
 #define DISPLAY1 1
 
+static const char bin7seg_lut[] = {
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111, // 9
+    0b01110111, // A
+    0b01111100, // b
+    0b00111001, // C
+    0b01011110, // d
+    0b01111001, // E
+    0b01110001  // F
+};
+
 void configure_io_ports(void)
 {
     // Configure the GPIOs for the 7-segment display
@@ -53,58 +72,20 @@ void configure_io_ports(void)
     gpio_set_direction(DISPLAY1, GPIO_MODE_OUTPUT);
 }
 
-char digit_to_segment(char digit)
+void display_digits(char counter, char decimal_point)
 {
-    switch (digit)
-    {
-        case 0x0:
-            return 0b00111111;
-        case 0x1:
-            return 0b00000110;
-        case 0x2:
-            return 0b01011011;
-        case 0x3:
-            return 0b01001111;
-        case 0x4:
-            return 0b01100110;
-        case 0x5:
-            return 0b01101101;
-        case 0x6:
-            return 0b01111101;
-        case 0x7:
-            return 0b00000111;
-        case 0x8:
-            return 0b01111111;
-        case 0x9:
-            return 0b01101111;
-        case 0xa:
-            return 0b01110111;
-        case 0xb:
-            return 0b01111100;
-        case 0xc:
-            return 0b00111001;
-        case 0xd:
-            return 0b01011110;
-        case 0xe:
-            return 0b01111001;
-        case 0xf:
-            return 0b01110001;
-        default:
-            return 0b00000000;
-    }
-}
+    // Active display flag
+    static char active_display = 0;
 
-static char active_display = 0;
-void swap_display(void)
-{
+    // Swap the active display
     active_display = !active_display;
     gpio_set_level(DISPLAY0, !active_display);
     gpio_set_level(DISPLAY1, active_display);
-}
 
-void display_digit(char digit)
-{
-    char segment = digit_to_segment(digit);
+    // Get the 7-segment code for the digit according to which display is active
+    char segment = bin7seg_lut[active_display ? (counter >> 4) : (counter & 0x0F)];
+    
+    // Set the GPIOs for the 7-segment display
     gpio_set_level(A, (segment & 0b0000001));
     gpio_set_level(B, (segment & 0b0000010));
     gpio_set_level(C, (segment & 0b0000100));
@@ -112,11 +93,9 @@ void display_digit(char digit)
     gpio_set_level(E, (segment & 0b0010000));
     gpio_set_level(F, (segment & 0b0100000));
     gpio_set_level(G, (segment & 0b1000000));
-}
 
-void display_decimal_point(char state)
-{
-    gpio_set_level(DP, state);
+    // Set the GPIOs for the decimal point
+    gpio_set_level(DP, active_display ? (decimal_point >> 1) : (decimal_point & 0b1));
 }
 
 void app_main(void)
@@ -125,24 +104,18 @@ void app_main(void)
     configure_io_ports();
     char counter = 0; // Counter value
     short int timer = 0; // Timer value
-    char decimal_point = 0b00;
+    char decimal_point = 0b00; // Decimal point states
     
     while (1) {
         // At every 100 loops (100 * 10ms = 1s), increment the counter
-        if (timer++ == 100) {
+        if (++timer == 100) {
             timer = 0;
-            counter += 0b1;
+            counter++;
         }
 
-        // Display the counter value on the 7-segment display
-        display_digit(active_display ? (counter >> 4) : (counter & 0x0F));        
+        // Display the counter value on the 7-segment display 
+        display_digits(counter, decimal_point);        
 
-        // Display the decimal point if needed
-        display_decimal_point(active_display ? (decimal_point >> 1) : (decimal_point & 0b1));
-
-        // Repeat at 100Hz rate
-        swap_display();
-        
         // 1 second delay
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
