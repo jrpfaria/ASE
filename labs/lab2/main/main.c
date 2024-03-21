eccfdd035f015f07222751d3ca4b35c90c4bf9f5
Rodrigo Faria <jrfaria@ua.pt>
2024-03-21 14:14:01 +0000

    working

--- labs/lab2/main/main.c
+++ labs/lab2/main/main.c
@@ -2,7 +2,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "esp_log.h"/
+#include "esp_log.h"
 #include "sdkconfig.h"
 #define z(a) {gpio_reset_pin(a);gpio_set_direction(a,GPIO_MODE_OUTPUT);}
 #define s(a,b) {gpio_set_level(a,b);}
@@ -14,5 +14,5 @@
 #define w vTaskDelay(10/portTICK_PERIOD_MS);
 
 c* g="	";c* l="?[Ofm}ow|9^yq";i j;v h(v){f z(x)}
-c y=0;v p(c d){y=!y;f if(j<8)s(x,(((y?(d>>4):(d&15))[l])&(1<<j)))else s(x,(j>9?y:!y))}
+c y=0;v p(c d){y=!y;f if(j<8)s(x,(((y?(d>>4):(d&15))[l])&(1<<j)))else s(x,(j>8?y:!y))}
 v app_main(v){h();c q=0;i t=0;for(;;){if(t++==99){t=0;q++;};p(q);w}}
