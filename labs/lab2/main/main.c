96204f9c2823505ae8f7eb9f18bba130e7f41905
Rodrigo Faria <74474900+jrpfaria@users.noreply.github.com>
2024-04-09 11:34:48 +0100

    unnecessary bytes removed

--- labs/lab2/main/main.c
+++ labs/lab2/main/main.c
@@ -9,10 +9,9 @@
 #define f for(j=0;j<10;j++)
 #define x (j[g])-1
 #define v void
-#define i int
 #define c char
 #define w vTaskDelay(10/portTICK_PERIOD_MS);
 
-c* g="	";c* l="?[Ofm}ow|9^yq";i j;v h(v){f z(x)}
+c* g="	";c* l="?[Ofm}ow|9^yq";c j;v h(v){f z(x)}
 c y=0;v p(c d){y=!y;f if(j<8)s(x,(((y?(d>>4):(d&15))[l])&(1<<j)))else s(x,(j>8?y:!y))}
-v app_main(v){h();c q=0;i t=0;for(;;){if(t++==99){t=0;q++;};p(q);w}}
+v app_main(v){h();c q=0;c t=0;for(;;){if(t++==99)t=0,q++;p(q);w}}
